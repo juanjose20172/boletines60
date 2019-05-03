826e0ec34028c23c27eb6da6fedd43d5b80eab64
jjgasa01@gmail.com <jjgasa01@gmail.com>
2019-05-03 17:23:05 -0500

    coment

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,31 @@
+var gulp = require('gulp');
+var server = require('gulp-server-livereload');
+
+
+uglify = require('gulp-uglify'),
+minifyCss = require('gulp-clean-css');
+ 
 gulp.task('server', function() {
-    gulp.src('./')
-      .pipe(server({
-        livereload: true,
-        directoryListing: true,
-        open: true
-      }));
+  gulp.src('./')
+    .pipe(server({
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+});
+
+gulp.task('build-html', function () {
+  return gulp.src(['*.html', 'pa*/*.html'])
+      .pipe(useref())
+      .pipe(gulpif('*.js', uglify()))
+      .pipe(gulpif('cs*/*.css', minifyCss()))
+      .pipe(gulp.dest('build'));
   });
+
+  gulp.task('build-assets-img', function () {
+    return gulp.src('asset*/**/**').
+      pipe(gulp.dest('build'))
+    })
+  
+    
+  gulp.task('build', ['build-html','build-assets-img'])
